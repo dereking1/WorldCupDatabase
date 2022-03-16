0150d9f7ea3ed94a811f5ab5f8625651e5f04a41
dereking1 <75998934+dereking1@users.noreply.github.com>
2022-03-16 15:12:26 -0700

    Add files via upload

--- load-data.sql
+++ load-data.sql
@@ -2,38 +2,20 @@
 
 SET GLOBAL local_infile = 1;
 
-LOAD DATA LOCAL INFILE 'Users/Daniel/Desktop/caltech/soph/q2/cs121/final/data/tournaments.csv' INTO TABLE tournaments
+LOAD DATA LOCAL INFILE 'data/tournaments.csv' INTO TABLE tournaments
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE 'Users/Daniel/Desktop/caltech/soph/q2/cs121/final/data/team.csv' INTO TABLE team
+LOAD DATA LOCAL INFILE 'data/team.csv' INTO TABLE team
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE 'Users/Daniel/Desktop/caltech/soph/q2/cs121/final/data/player.csv' INTO TABLE player
+LOAD DATA LOCAL INFILE 'data/player.csv' INTO TABLE player
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE 'Users/Daniel/Desktop/caltech/soph/q2/cs121/final/data/squads.csv' INTO TABLE squad
+LOAD DATA LOCAL INFILE 'data/squads.csv' INTO TABLE squad
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE 'Users/Daniel/Desktop/caltech/soph/q2/cs121/final/data/matches.csv' INTO TABLE matches
+LOAD DATA LOCAL INFILE 'data/matches.csv' INTO TABLE matches
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE 'Users/Daniel/Desktop/caltech/soph/q2/cs121/final/data/match_goals.csv' INTO TABLE match_goals
+LOAD DATA LOCAL INFILE 'data/match_goals.csv' INTO TABLE match_goals
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
-
--- LOAD DATA LOCAL INFILE 'data/tournaments.csv' INTO TABLE tournaments
--- FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
-
--- LOAD DATA LOCAL INFILE 'data/team.csv' INTO TABLE team
--- FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
-
--- LOAD DATA LOCAL INFILE 'data/player.csv' INTO TABLE player
--- FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
-
--- LOAD DATA LOCAL INFILE 'data/squads.csv' INTO TABLE squad
--- FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
-
--- LOAD DATA LOCAL INFILE 'data/matches.csv' INTO TABLE matches
--- FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
-
--- LOAD DATA LOCAL INFILE 'data/match_goals.csv' INTO TABLE match_goals
--- FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
